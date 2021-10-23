5cb6966acf3708a76533f849380c86aa9bd1261d
Sergey Mikhaylin <sergey.i.mikhaylin@tele2.ru>
2021-10-23 16:51:17 +0300

    Запрос исторических данных по названию валюты

--- tinvest_analysis/loaders/cbrf_api.py
+++ tinvest_analysis/loaders/cbrf_api.py
@@ -1,17 +1,30 @@
 import datetime as dt
+from enum import Enum
 
 import pandas as pd
 from cbrf.models import DynamicCurrenciesRates
 
 
-def currency_history_quotes(code: str, from_date: dt.date, to_date: dt.date):
+class CurrencyID(Enum):
+    USD = "R01235"
+    EUR = "R01239"
+    GBP = "R01035"
+    CHF = "R01775"
+    CNY = "R01375"
+    JPY = "R01820"
+    HKD = "R01200"
+    TRY = "R01700"
+
+
+def currency_history_quotes(name: str, from_date: dt.date, to_date: dt.date):
     # USD: R01235
     # EUR: R01239
     # see: https://www.cbr.ru/scripts/XML_val.asp?d=0
     # делаем запрос к ЦБРФ для получения исторических данных
     from_date = dt.datetime(from_date.year, from_date.month, from_date.day)
     to_date = dt.datetime(to_date.year, to_date.month, to_date.day)
-    history_rates = DynamicCurrenciesRates(from_date, to_date, code)
+    id_code = CurrencyID[name].value
+    history_rates = DynamicCurrenciesRates(from_date, to_date, id_code)
     # формируем и возвращаем результат
     rate_dates = [rate.date for rate in history_rates.rates]
     rate_values = [rate.value / rate.denomination for rate in history_rates.rates]
@@ -21,6 +34,6 @@ def currency_history_quotes(code: str, from_date: dt.date, to_date: dt.date):
 
 if __name__ == '__main__':
     # пример запроса исторического курса доллара
-    date_1 = dt.datetime(2021, 9, 1)
-    date_2 = dt.datetime(2021, 9, 30)
-    usd_history = currency_history_quotes('R01235', date_1, date_2)
+    date_to = dt.date.today()
+    date_from = date_to - dt.timedelta(days=30)
+    usd_history = currency_history_quotes('USD', date_from, date_to)
